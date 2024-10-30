943712c09b0b99aa4c700e5bbfa05350af80c4f7
cs-mg-gh <145803911+cs-mg-gh@users.noreply.github.com>
2024-10-30 12:44:33 -0700

    new repository, but image is not rounded.

---
+++ hello_flask.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+Bootstrap5()  # Initialize Bootstrap5 directly with the app instance
+
+@app.route('/')
+def index():
+    return render_template('template.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
